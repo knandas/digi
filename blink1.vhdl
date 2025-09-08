99f4c07f25c09053bcc9b339c87cf82c6a29ae9f
knandas <nandhasri.k@gmail.com>
2025-09-08 21:21:20 +0700

    Update blink1.vhdl

--- blink1.vhdl
+++ blink1.vhdl
@@ -11,3 +11,23 @@ module counter(clk, out);
      out <= count[23];
   end
 endmodule    
+
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+//////////////////////////////// slower_clock.v //////////////////////////////////////////////////////
+    
+module slower_clock(input clk, output reg clkOutput);
+
+reg [22:0] counter;
+
+always @(posedge clk) 
+    if(counter==5000000) counter <= 0; else counter <= counter+1;
+
+always @(posedge clk) 
+    if(counter==5000000) 
+        begin
+            clkOutput <= ~clkOutput;
+        end
+endmodule
+
+/////////////////////////////
