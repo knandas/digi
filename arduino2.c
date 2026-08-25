ba12b21ef3ec5da4c09c1e6c1ad4255060f8b716
knandas <nandhasri.k@gmail.com>
2026-08-25 09:12:31 +0700

    Update arduino2.c

--- arduino2.c
+++ arduino2.c
@@ -1,6 +1,27 @@
-//////////////////////// night rider////////////////////////////////////
-YLXeK3
-//////////////////////// night rider////////////////////////////////////
+//////////////////////// vscode wokwi////////////////////////////////////
+https://www.youtube.com/watch?v=ECNTyMm_5PE
+https://docs.wokwi.com/vscode/project-config
+
+1. install platformIO extension in vscode
+2. install wokwi extension in vscode
+
+- click on platformIO creat new project  edit "/src/main.c" then complile.
+- press F1 select "wokwi:request a new licence. open link then click "get your licence"
+  
+- create a new file "diagram.json". right click on "diagram.json" select open with text editor default.
+- copy "diagram.json" from wokwi to vscode.
+
+- create a new "wokwi.toml" file
+[wokwi]
+version = 1
+firmware = 'path-to-your-firmware.hex'
+elf = 'path-to-your-firmware.elf'
+
+- copy path to .toml file  
+
+finish
+
+////////////////////////KITT knight rider///////////////////YLXeK3/////////////////
 
 int led7 = 13;
 int led6 = 12;
